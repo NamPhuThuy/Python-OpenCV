cd715e72a9191faaf231caf53f699f3f4e35ce78
Kryple <ttnam957@gmail.com>
2024-08-23 14:49:01 +0700

    Add: encode and decode run-length

--- Part01 - Introduction/ShowImageAndGrayScaleVersion.py
+++ Part01 - Introduction/ShowImageAndGrayScaleVersion.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 def load_image(path):
     # cv2.imread(): read an image.
     # The image should be in the working directory or a full path of image should be provided.
@@ -12,7 +13,6 @@ def show_image(image):
     # First argument: window name.
     # Second argument: image to be displayed.
     # Each created window should have different window names.
-    image = cv2.resize(image, (400, 300))
     cv2.imshow("image", image)
 
     # cv2.waitKey() is a keyboard binding function.
@@ -25,24 +25,45 @@ def show_image(image):
 
     # To destroy all the windows we created
     cv2.destroyAllWindows()
-    
-
-def convert_to_grayscale(image):
-    # Use cv2.cvtColor() to convert an image from one color format to another
-    # In this case we use cv2.cvtColor() to convert the loaded image to grayscale (BGR to GRAY):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 
 def write_image_to_disk(path, image):
     """this function writes to disk an image given the image to be written"""
     cv2.imwrite(path, image)
+    
+def show_gray_scale_version(image):
+    # Use cv2.cvtColor() to convert an image from one color format to another
+    # In this case we use cv2.cvtColor() to convert the loaded image to grayscale (BGR to GRAY):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray scale Image', gray_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+def show_binary_version(image): 
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+    # Display the binary image
+    cv2.imshow('Binary Image', thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    bgr_image = load_image("../images/wukong.jpg")
-    show_image(bgr_image)
+    image = load_image("../images/wukong.jpg")
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # binary_image = cv2.bitwise_and(image, image, mask=gray_image)
+    binary_image = gray_image > 0.6
     
-    gray_image = convert_to_grayscale(bgr_image)
-    show_image(gray_image)
-
-    write_image_to_disk("../images/gray_wukong.jpg", gray_image)
+    
+    # image = cv2.resize(image, (400, 300))
+    
+    show_image(image)
+    show_gray_scale_version(image)
+    show_binary_version(image)
+    
+    
+    # binary_matrix = binary_image.astype(np.uint8)
+    # np.set_printoptions(threshold=np.inf)
+    # print(binary_matrix)
+    
+    # write_image_to_disk("../images/gray_wukong.jpg", image)
