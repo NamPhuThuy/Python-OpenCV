b30085778e98ca55fbf3fa714362210e566d7299
Kryple <ttnam957@gmail.com>
2024-08-23 15:23:23 +0700

    Modiy: update Part04

--- Part04/02_basic_drawing_p2.py
+++ Part04/02_basic_drawing_p2.py
@@ -1,14 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Dictionary containing some colors:
-colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 
-          'red': (0, 0, 255), 'yellow': (0, 255, 255),
-          'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 
-          'white': (255, 255, 255), 'black': (0, 0, 0),
-          'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
-          'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
+import constant_colors
 
 def show_with_matplotlib(img, title):
     # Convert BGR image to RGB:
@@ -31,8 +24,8 @@ def cliplines_example():
     rectp2 = (100, 100)
     
     # Draw a rectangle and a line:
-    cv2.line(image, linep1, linep2, colors['green'], 3)
-    cv2.rectangle(image, rectp1, rectp2, colors['blue'], 3)
+    cv2.line(image, linep1, linep2, constant_colors.GREEN, 3)
+    cv2.rectangle(image, rectp1, rectp2, constant_colors.BLUE, 3)
     
     # We call the function cv2.clipLine():
     ret, p1, p2 = cv2.clipLine((rectp1 + rectp2), linep1, linep2)
@@ -41,32 +34,32 @@ def cliplines_example():
     # cv2.clipLine() returns False if the line is outside the rectangle
     # And returns True otherwise
     if ret:
-        cv2.line(image, p1, p2, colors['yellow'], 3)
+        cv2.line(image, p1, p2, constant_colors.YELLOW, 3)
     
     show_with_matplotlib(image, 'cv2.clipLine()')
-    image[:] = colors['light_gray']
+    image[:] = constant_colors.LIGHT_GRAY
     
 def arrowedLine_example():
     # 2. We are going to see how cv2.arrowedLine() works:
-    cv2.arrowedLine(image, (50, 50), (200, 50), colors['red'], 3, 8, 0, 0.1)
-    cv2.arrowedLine(image, (50, 120), (200, 120), colors['green'], 3, cv2.LINE_AA, 0, 0.3)
-    cv2.arrowedLine(image, (50, 200), (200, 200), colors['blue'], 3, 8, 0, 0.3)
+    cv2.arrowedLine(image, (50, 50), (200, 50), constant_colors.RED, 3, 8, 0, 0.1)
+    cv2.arrowedLine(image, (50, 120), (200, 120), constant_colors.GREEN, 3, cv2.LINE_AA, 0, 0.3)
+    cv2.arrowedLine(image, (50, 200), (200, 200), constant_colors.BLUE, 3, 8, 0, 0.3)
     
     show_with_matplotlib(image, 'cv2.arrowedLine()')
     image[:] = colors['light_gray']
 
 def ellipse_example():    
     # 3. We are going to see how cv2.ellipse() works:
-    cv2.ellipse(image, (80, 80), (60, 40), 0, 0, 360, colors['red'], -1)
-    cv2.ellipse(image, (80, 200), (80, 40), 0, 0, 360, colors['green'], 3)
-    cv2.ellipse(image, (80, 200), (10, 40), 0, 0, 360, colors['blue'], 3)
-    cv2.ellipse(image, (200, 200), (10, 40), 0, 0, 180, colors['yellow'], 3)
-    cv2.ellipse(image, (200, 100), (10, 40), 0, 0, 270, colors['cyan'], 3)
-    cv2.ellipse(image, (250, 250), (30, 30), 0, 0, 360, colors['magenta'], 3)
-    cv2.ellipse(image, (250, 100), (20, 40), 45, 0, 360, colors['gray'], 3)
+    cv2.ellipse(image, (80, 80), (60, 40), 0, 0, 360, constant_colors.RED, -1)
+    cv2.ellipse(image, (80, 200), (80, 40), 0, 0, 360, constant_colors.GREEN, 3)
+    cv2.ellipse(image, (80, 200), (10, 40), 0, 0, 360, constant_colors.BLUE, 3)
+    cv2.ellipse(image, (200, 200), (10, 40), 0, 0, 180, constant_colors.YELLOW, 3)
+    cv2.ellipse(image, (200, 100), (10, 40), 0, 0, 270, constant_colors.CYAN, 3)
+    cv2.ellipse(image, (250, 250), (30, 30), 0, 0, 360, constant_colors.MAGENTA, 3)
+    cv2.ellipse(image, (250, 100), (20, 40), 45, 0, 360, constant_colors.GRAY, 3)
 
     show_with_matplotlib(image, 'cv2.ellipse()')
-    image[:] = colors['light_gray']
+    image[:] = constant_colors.LIGHT_GRAY
 
 def polylines_example():
     # 4. We are going to draw several polylines
@@ -77,7 +70,7 @@ def polylines_example():
     # Print the shapes: this line is not necessary, only for visualization:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with True option:
-    cv2.polylines(image, [pts], True, colors['green'], 3)
+    cv2.polylines(image, [pts], True, constant_colors.GREEN, 3)
     
     # These points define a triangle:
     pts = np.array([[250, 105], [220, 180], [280, 180]], np.int32)
@@ -86,7 +79,7 @@ def polylines_example():
     # Print the shapes:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with False option:
-    cv2.polylines(image, [pts], False, colors['green'], 3)
+    cv2.polylines(image, [pts], False, constant_colors.GREEN, 3)
     
     # These points define a pentagon:
     pts = np.array([[20, 90], [60, 60], [100, 90], [80, 130], [40, 130]], np.int32)
@@ -95,7 +88,7 @@ def polylines_example():
     # Print the shapes:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with True option:
-    cv2.polylines(image, [pts], True, colors['blue'], 3)
+    cv2.polylines(image, [pts], True, constant_colors.BLUE, 3)
     
     # These points define a pentagon:
     pts = np.array([[20, 180], [60, 150], [100, 180], [80, 220], [40, 220]], np.int32)
@@ -104,7 +97,7 @@ def polylines_example():
     # Print the shapes:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with False option:
-    cv2.polylines(image, [pts], False, colors['blue'], 3)
+    cv2.polylines(image, [pts], False, constant_colors.BLUE, 3)
     
     # These points define a rectangle:
     pts = np.array([[150, 100], [200, 100], [200, 150], [150, 150]], np.int32)
@@ -113,7 +106,7 @@ def polylines_example():
     # Print the shapes:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with False option:
-    cv2.polylines(image, [pts], True, colors['yellow'], 3)
+    cv2.polylines(image, [pts], True, constant_colors.YELLOW, 3)
     
     # These points define a rectangle:
     pts = np.array([[150, 200], [200, 200], [200, 250], [150, 250]], np.int32)
@@ -122,17 +115,41 @@ def polylines_example():
     # Print the shapes:
     print("shape of pts '{}'".format(pts.shape))
     # Draw this polygon with False option:
-    cv2.polylines(image, [pts], False, colors['yellow'], 3)
+    cv2.polylines(image, [pts], False, constant_colors.YELLOW, 3)
     
     show_with_matplotlib(image, 'cv2.polylines()')
-    image[:] = colors['light_gray']
+    image[:] = constant_colors.LIGHT_GRAY
+    
+def circle_polygon_example():
+    # We create the canvas to draw: 640 x 640 pixels, 3 channels, uint8 (8-bit unsigned integers)
+    # We set background to black using np.zeros():
+    image = np.zeros((640, 640, 3), dtype="uint8")
+    
+    # If you want another background color you can do the following:
+    # image[:] = colors['light_gray']
+    image.fill(255)
+    
+    # Create the necessary (12 in this case) points to build the circle polygon:
+    pts = np.array(
+        [(600, 320), (563, 460), (460, 562), (320, 600), (180, 563), (78, 460), (40, 320), (77, 180), (179, 78), (319, 40),
+         (459, 77), (562, 179)])
+    
+    # Reshape to shape (number_vertex, 1, 2):
+    pts = pts.reshape((-1, 1, 2))
+    
+    # Call cv2.polylines() to build the polygon:
+    cv2.polylines(image, [pts], True, constant_colors.GREEN, 5)
+    
+    # Show image:
+    show_with_matplotlib(image, 'polygon with the shape of a circle using 12 points')
 
 if __name__ == "__main__":
     image = np.zeros((300, 300, 3), dtype="uint8")
-    image[:] = colors['light_gray']
+    image[:] = constant_colors.LIGHT_GRAY
     
     # cliplines_example()
     # arrowedLine_example()
     # ellipse_example()
     # polylines_example()
+    circle_polygon_example()
     
